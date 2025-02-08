540fc7343941d253e33728c6bfda10511bb66b38
Faizan Anwar <manwar.bscs23seecs@seecs.edu.pk>
2025-02-08 18:23:34 +0500

    Made some changes to it

--- myapi/myapi/urls.py
+++ myapi/myapi/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+        path('', include('tasks.urls')),
+
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),  # Ensure this is correct
 ]
